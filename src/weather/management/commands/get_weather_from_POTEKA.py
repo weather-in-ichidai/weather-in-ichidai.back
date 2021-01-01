98f65ade179c63df1406cc4fe68c8c8e3b631b76
SansanM <52972918+SansanM@users.noreply.github.com>
2021-01-01 23:37:06 +0900

    get_weather_from_POTEKA にエラーハンドリングを追加

--- src/weather/management/commands/get_weather_from_POTEKA.py
+++ src/weather/management/commands/get_weather_from_POTEKA.py
@@ -26,28 +26,33 @@ class Command(BaseCommand):
         real_request = requests.get(real_url, params=real_payload,headers=headers)
         forecast_request = requests.get(forecast_url, params=forecast_payload,headers=headers)
 
-        real_res = real_request.json()
-        forecast_res = forecast_request.json()
-        temp_array = []
+        #通信が成功したかの判定
+        try:
+            real_request.raise_for_status()
+            forecast_request.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print("error :",e)
+        else:
+            real_res = real_request.json()
+            forecast_res = forecast_request.json()
+            temp_array = []
         
+            #温度の予測値を24時間後まで配列に格納
+            for x in forecast_res["poteka"][0]["element"][0]["dataList"][0:24]:
+                temp_array.append(str(x["value"]))
 
-        for x in forecast_res["poteka"][0]["element"][0]["dataList"][0:24]:
-            temp_array.append(str(x["value"]))
+            #雨が降って無ければ0
+            now_rain = 1
+            if real_res["poteka"][0]["element"][0]["dataList"][0]["value"] == "false":
+                now_rain = 0 
 
-        #雨が降って無ければ0
-        now_rain = 1
+            #データの保存
+            data = Weather_data()
+            data.date_time = real_res["poteka"][0]["element"][0]["dataList"][0]["datatime"]
+            data.temp = ",".join(temp_array)
+            data.pop = forecast_res["poteka"][0]["element"][1]["dataList"][0]["value"]
+            data.weather = forecast_res["poteka"][0]["element"][2]["dataList"][0]["value"]
+            data.now_rain = now_rain
+            data.save()
 
-        if real_res["poteka"][0]["element"][0]["dataList"][0]["value"] == "false":
-            now_rain = 0 
-
-
-        print(temp_array)
-        data = Weather_data()
-        data.date_time = real_res["poteka"][0]["element"][0]["dataList"][0]["datatime"]
-        data.temp = ",".join(temp_array)
-        data.pop = forecast_res["poteka"][0]["element"][1]["dataList"][0]["value"]
-        data.weather = forecast_res["poteka"][0]["element"][2]["dataList"][0]["value"]
-        data.now_rain = now_rain
-        data.save()
-
-        print("cleate data")
+            print("cleate data")
